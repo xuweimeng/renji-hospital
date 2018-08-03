089ed6f9b22161531a993581d58cdffe9bd009e2
yugou <yugou@myaidoctor.com>
2018-08-03 18:40:14 +0800

    0803 :bug: 修复患者档案，某个日期记录多条时也只显示仅一条的bug，样式待再优化

--- src/views/HN_DoctorClient/common/patientFile.vue
+++ src/views/HN_DoctorClient/common/patientFile.vue
@@ -61,6 +61,9 @@
       color: #409eff;
       margin-bottom: 5px;
       font-weight: 400;
+      margin-top: 15px;
+    }
+    &_name:first-child{
       margin-top: 0;
     }
     &_link{
@@ -157,21 +160,22 @@
           <el-button v-else   icon="el-icon-star-off" class="record_header_cancel"  size="mini" type="primary" @click="addSpecial" >添加关注</el-button>
       </div>
       <el-tabs @tab-click="currentPartientInfo" v-model="currentTable"  type="border-card" v-if="timeList[0]">
-          <el-tab-pane  v-for="(item,index) in timeList" :key="index" :name="item.diagnosetime" >
-                <h6 class="record_table_name" slot="label"><span>{{item.mzOrzy=='1'?'门诊':'住院'}}</span> {{item.diagnosetime}}</h6>
-                <template v-if="item.mzOrZy!='mz' && item.adminPatientDiagnose">
+          <el-tab-pane  v-for="(itemMain,index) in timeList" :key="index" :name="itemMain.diagnosetime" >
+                <h6 class="record_table_name" slot="label"><span>{{itemMain.mzOrzy=='1'?'门诊':'住院'}}</span> {{itemMain.diagnosetime}}</h6>
+            <template v-for="(item,index) in itemMain">
+              <template v-if="item.mzOrZy!='mz' && item.adminPatientDiagnose">
                 <h5 class="record_content_name">就诊信息
-                  <h6 class="record_content_link" v-if="item.isHasVisit=='1'" @click="sfDialog(item.adminPatientDiagnose.taskId)">查看随访记录</h6>
+                  <h6 class="record_content_link" v-if="itemMain.isHasVisit=='1' && item.adminPatientDiagnose.taskId" @click="sfDialog(item.adminPatientDiagnose.taskId)">查看随访记录</h6>
                 </h5>
                 <!-- 有随访记录展示随访记录 -->
                 <ul class="record_content_list">
                   <li  class="record_content_single">
                     就诊卡号:{{item.mzOrZyNum}}
                   </li>
-                    <li  class="record_content_single">
+                  <li  class="record_content_single">
                     患者性质:{{item.brxz}}
                   </li>
-                    <li class="record_content_single">
+                  <li class="record_content_single">
                     入院时间:{{item.znjqrCyxjList[0].admissiontime}}
                   </li>
                   <li class="record_content_single">
@@ -187,58 +191,59 @@
                 <h5 class="record_content_msg">出院小结</h5>
                 <div class="record_content_box">
                   <el-steps class="record_content_step" v-if="item.znjqrCyxjList.length>0" direction="vertical" :active="item.znjqrCyxjList.length">
-                      <el-step v-for="ite in item.znjqrCyxjList"  :key="ite.id" :title="ite.admissiontime">
-                        <div slot="description">
-                          <p class="record_content_text" v-for="discharged in Object.keys(dischargedMap)"  :key="discharged">
-                            <el-tag type="warning">
-                              {{dischargedMap[discharged]}}
-                            </el-tag>
-                            {{ite[discharged]}}
-                          </p>
-                        </div>
-                      </el-step>
-                    </el-steps>
+                    <el-step v-for="ite in item.znjqrCyxjList"  :key="ite.id" :title="ite.admissiontime">
+                      <div slot="description">
+                        <p class="record_content_text" v-for="discharged in Object.keys(dischargedMap)"  :key="discharged">
+                          <el-tag type="warning">
+                            {{dischargedMap[discharged]}}
+                          </el-tag>
+                          {{ite[discharged]}}
+                        </p>
+                      </div>
+                    </el-step>
+                  </el-steps>
                 </div>
-                </template>
-                <template v-if="item.mzOrZy!='zy' && item.adminPatientDiagnose">
-                  <h5 class="record_content_name">就诊信息
-                    <h6 class="record_content_link" v-if="item.isHasVisit=='1'" @click="sfDialog(item.adminPatientDiagnose.taskId)">查看随访记录</h6>
-                  </h5>
-                  <!-- 有随访记录展示随访记录 -->
+              </template>
+              <template v-if="item.mzOrZy!='zy' && item.adminPatientDiagnose">
+                <h5 class="record_content_name">就诊信息
+                  <h6 class="record_content_link" v-if="itemMain.isHasVisit=='1' && item.adminPatientDiagnose.taskId" @click="sfDialog(item.adminPatientDiagnose.taskId)">查看随访记录</h6>
+                </h5>
+                <!-- 有随访记录展示随访记录 -->
 
-                  <ul class="record_content_list">
-                    <li  class="record_content_single">
-                      就诊卡号:{{item.mzOrZyNum}}
-                    </li>
-                      <li  class="record_content_single">
-                      患者性质:{{item.brxz}}
-                    </li>
-                      <li class="record_content_single">
-                      医生姓名:{{item.adminName}}
-                    </li>
-                    <li class="record_content_single">
-                      科室:{{item.adminDepartment}}
-                    </li>
-                    <li class="record_content_single">
-                      疾病诊断:{{item.adminPatientDiagnose.icdName}}
-                    </li>
-                    <li class="record_content_single">
-                      主诉:{{item.zs}}
+                <ul class="record_content_list">
+                  <li  class="record_content_single">
+                    就诊卡号:{{item.mzOrZyNum}}
+                  </li>
+                  <li  class="record_content_single">
+                    患者性质:{{item.brxz}}
+                  </li>
+                  <li class="record_content_single">
+                    医生姓名:{{item.adminName}}
+                  </li>
+                  <li class="record_content_single">
+                    科室:{{item.adminDepartment}}
+                  </li>
+                  <li class="record_content_single">
+                    疾病诊断:{{item.adminPatientDiagnose.icdName}}
+                  </li>
+                  <li class="record_content_single">
+                    主诉:{{item.zs}}
+                  </li>
+                </ul>
+                <h5 class="record_content_msg">处方信息</h5>
+                <div class="record_content_box">
+                  <ul class="record_content_content" v-for="(ite,inde) in item.znjqrCfmxList" :key="inde">
+                    <h6>药品名称:{{ite.ypmc}}</h6>
+                    <li class="record_content_param" v-for="(it,ind) in Object.keys(medicineMap)" :key="ind">
+                      <el-tag type="warning">
+                        {{medicineMap[it]}}
+                      </el-tag>
+                      {{ite[it]}}
                     </li>
                   </ul>
-                  <h5 class="record_content_msg">处方信息</h5>
-                  <div class="record_content_box">
-                      <ul class="record_content_content" v-for="(ite,inde) in item.znjqrCfmxList" :key="inde">
-                        <h6>药品名称:{{ite.ypmc}}</h6>
-                        <li class="record_content_param" v-for="(it,ind) in Object.keys(medicineMap)" :key="ind">
-                          <el-tag type="warning">
-                            {{medicineMap[it]}}
-                          </el-tag>
-                          {{ite[it]}}
-                        </li>
-                      </ul>
-                  </div>
-                </template>
+                </div>
+              </template>
+            </template>
           </el-tab-pane>
       </el-tabs>
     </el-dialog>
@@ -260,47 +265,47 @@
  * 患者档案
  * @module patientFile
  */
-import { PatientFile } from "HNDC_API/common/patientFile";
-import mixin from "@/assets/HN_DoctorClient/js/mixin";
-import { mapGetters } from "vuex";
-import followRecord from "HNDC/common/followRecord";
+import { PatientFile } from 'HNDC_API/common/patientFile';
+import mixin from '@/assets/HN_DoctorClient/js/mixin';
+import { mapGetters } from 'vuex';
+import followRecord from 'HNDC/common/followRecord';
 export default {
   data() {
     return {
-      currentTable: "",
+      currentTable: '',
       // 药物字典
       medicineMap: {
-        yfgg: "规格",
-        ypcd: "产地",
-        yspl: "数量",
-        yfdw: "单位",
-        ycjl: "剂量",
-        jldw: "剂量单位",
-        yyts: "使用天数",
-        mrcs: "每日次数",
-        cflx: "频次",
-        yf: "用法"
+        yfgg: '规格',
+        ypcd: '产地',
+        yspl: '数量',
+        yfdw: '单位',
+        ycjl: '剂量',
+        jldw: '剂量单位',
+        yyts: '使用天数',
+        mrcs: '每日次数',
+        cflx: '频次',
+        yf: '用法'
       },
-      //出院小结字典
+      // 出院小结字典
       dischargedMap: {
-        admissiondiagnose: "入院诊断",
-        admissiondescription: "入院情况",
-        cureprocess: "诊治经过",
-        filedescription: "转归情况",
-        leavediagnose: "出院诊断",
-        leavedescription: "出院情况",
-        leavedoctorcharge: "出院医嘱"
+        admissiondiagnose: '入院诊断',
+        admissiondescription: '入院情况',
+        cureprocess: '诊治经过',
+        filedescription: '转归情况',
+        leavediagnose: '出院诊断',
+        leavedescription: '出院情况',
+        leavedoctorcharge: '出院医嘱'
       },
       timeList: [{
-        znjqrCyxjList:[{
+        znjqrCyxjList: [{
 
         }],
-        znjqrCfxx:{},
-        adminPatientDiagnose:{},
+        znjqrCfxx: {},
+        adminPatientDiagnose: {}
       }],
       baseData: {}, // 患者基本信息
       dialogVisible: false, // 患者档案弹框是否显示
-      taskIdRecord: "", // 传给随访记录的taskid
+      taskIdRecord: '' // 传给随访记录的taskid
     };
   },
   props: {
@@ -326,7 +331,7 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(["token"])
+    ...mapGetters(['token'])
   },
   // 含getPatientInfo,handleislike两个方法
   mixins: [mixin],
@@ -342,7 +347,7 @@ export default {
       this.getPatientInfo().then(res => {
         this.baseData = res.data;
       });
-      this.$emit("refreshData");
+      this.$emit('refreshData');
     },
     // 有 随访记录 按钮时调用
     sfDialog(taskId) {
@@ -370,9 +375,9 @@ export default {
       *请求当前时间的患者信息
       */
     currentPartientInfo(obj) {
-      this.timeList.forEach(item=>{
-        if(item.diagnosetime==obj.name){
-          if(item.znjqrCyxjList){
+      this.timeList.forEach(item => {
+        if (item.diagnosetime == obj.name) {
+          if (item.znjqrCyxjList) {
             return false;
           }
         }
@@ -383,15 +388,20 @@ export default {
         date: obj.name
       })
         .then(res => {
-            this.timeList.forEach((item,index)=>{
-              if(item.diagnosetime==obj.name){
-                if(item.znjqrCyxjList){
-                  return false;
-                }
-                item=Object.assign(item,res.data[0]);
-                this.timeList.splice(index,1,item);
+          this.timeList.forEach((item, index) => {
+            if (item.diagnosetime == obj.name) {
+              if (item.znjqrCyxjList) {
+                return false;
               }
-            });
+              // item = Object.assign(item, res.data[0]);
+              const per_date_arr = [];
+              res.data.forEach(item => {
+                per_date_arr.push(item);
+              });
+              item = Object.assign(item, per_date_arr);
+              this.timeList.splice(index, 1, item);
+            }
+          });
         })
         .catch(error => {
           console.log(error);
@@ -407,13 +417,13 @@ export default {
         patientId: this.patientId
       })
         .then(res => {
-            this.timeList = res.data;
-            if (this.timeList.length) {
-              this.currentTable=this.timeList[0].diagnosetime;
-              this.currentPartientInfo({
-                name:this.timeList[0].diagnosetime
-              });
-            }
+          this.timeList = res.data;
+          if (this.timeList.length) {
+            this.currentTable = this.timeList[0].diagnosetime;
+            this.currentPartientInfo({
+              name: this.timeList[0].diagnosetime
+            });
+          }
         })
         .catch(error => {
           console.log(error);
